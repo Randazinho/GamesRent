45313404f4e3b4c179a9e7e424eab1b6e45d9d7a
Flavio Randazzo <flavio.randazzo@condorcet.be>
2023-06-24 15:28:15 +0200

    Maj Game

--- GamesRent/Classes/Game.cs
+++ GamesRent/Classes/Game.cs
@@ -64,4 +64,65 @@ public class Game
     {
         this.copys.Remove(copy);
     }
+
+    public void UpdateCostByID(int id_game, int cr_cost)
+    {
+        GameDAO GameDao = new GameDAO();
+        GameDao.UpdateCostByID(id_game, cr_cost);
+    }
+
+    public static Game Find(int id_game)
+    {
+        GameDAO GameDao = new GameDAO();
+        return GameDao.Find(id_game);
+    }
+
+    public static List<Game> FindGameByName(string name, List<Game> Games)
+    {
+        GameDAO GameDao = new GameDAO();
+        return GameDao.FindGameByName(name, Games);
+    }
+
+    public static List<Game> FindAllGame(List<Game> Games)
+    {
+        GameDAO GameDao = new GameDAO();
+        return GameDao.FindAllGame(Games);
+    }
+
+    public static List<Game> FindGameByConsole(string console, List<Game> Games)
+    {
+        GameDAO GameDao = new GameDAO();
+        return GameDao.FindGameByConsole(console,Games);
+    }
+
+    public static List<Game> FindGameByCrCost(int crCost, List<Game> Games)
+    {
+        GameDAO GameDao = new GameDAO();
+        return GameDao.FindGameByCrCost(crCost, Games);
+    }
+
+    public void CreateGameByAdmin(string name, int creditCost, string console)
+    {
+        GameDAO GameDao = new GameDAO();
+        GameDao.CreateGameByAdmin(name, creditCost,console);
+    }
+
+    public void DeleteGame(int id_game)
+    {
+        GameDAO GameDao = new GameDAO();
+        GameDao.DeleteGame(id_game);
+    }
+
+    public static int CopyAvailable(int id_game, int id_player, int id_booking, int week)
+    {
+        GameDAO GameDao = new GameDAO();
+        return GameDao.CopyAvailable(id_game,id_player,id_booking,week);
+    }
+
+    public int SelectBooking(int idgame)
+    {
+        GameDAO GameDao = new GameDAO();
+        return GameDao.SelectBooking(id_game);
+    }
+
 }
